036252c0b76f40abee032d11c0d43dc1dda71f70
master <rudolifrudolif@gmail.com>
2020-04-30 00:54:18 +0300

    page one

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+//------------------------slider-hero-----------------------------------
+	var swiper = new Swiper('.hero', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		effect: 'fade',
+		// autoplay: {
+		// 	delay: 5500,
+		// 	disableOnInteraction: false,
+		// },
+		navigation: {
+			nextEl: '.hero__button_next',
+			prevEl: '.hero__button_prev',
+		},
+	});
+
+//------------------------slider-hero-----------------------------------
+	var swiper = new Swiper('.partners__slider', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		navigation: {
+			nextEl: '.partners__button_next',
+			prevEl: '.partners__button_prev',
+		},
+		breakpoints: {
+			480: {
+				slidesPerView: 2,
+			},
+			992: {
+				slidesPerView: 1,
+				spaceBetween: 20
+			},
+		}
+	});
+
 //-------------------------------попандер---------------------------------------
 	// $('.modal').popup({transition: 'all 0.3s'});
 
 //------------------------------гамбургер-----------------------------
-	// $('.hamburger').click(function() {
-	//   $(this).toggleClass('hamburger--active');
-	//   $('nav').toggleClass('nav--active');
-	//   $('header').toggleClass('header--menu');
-	// });
+	$('.hamburger').click(function() {
+	  $(this).toggleClass('hamburger--active');
+	  $('.nav').toggleClass('nav--active');
+	  $('body').toggleClass('no-scroll');
+	});
 
 //------------------------------------form-------------------------------------------
 	// $('input[type="tel"]').mask('+0 (000) 000-00-00');
